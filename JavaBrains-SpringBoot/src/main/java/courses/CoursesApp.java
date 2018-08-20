975492d9db6e7781580289f4eb61edf335271c68
Kapil Arora <kapil.arora@lakshyanet.com>
2018-08-20 19:45:47 +0530

    Spring Boot Basic

---
+++ JavaBrains-SpringBoot/src/main/java/courses/CoursesApp.java
@@ -0,0 +1,13 @@
+package courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesApp.class, args);
+	}
+
+}
